4b08e56a16b9e904f2af197a68e3d59b9d50cd81
Kaiano Levine <kaiano.levine@gmail.com>
2026-04-03 17:01:33 -0500

    fix: clerk login setup

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
-
-const isNotesRoute = createRouteMatcher(["/notes", "/notes/(.*)"]);
-
-export const onRequest = clerkMiddleware((auth, context) => {
-  if (!isNotesRoute(context.request)) return;
-});
